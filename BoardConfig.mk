07bb1e348b788929b59bd16ac6bbacf4085ee260
weiziheng <weiziheng@allwinnertech.com>
2011-11-22 19:32:26 +0800

    add nano and usb wifi support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,3 +6,11 @@
 include device/softwinner/crane-common/BoardConfigCommon.mk
 
 SW_TARGET_NO_TSCalibration := true
+
+# Wifi related defines
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+
+# Wifi chipset select
+# usb wifi "rtl8192cu"; sdio wifi "nanowifi"/"ar6302"/"usibcm4329"
+SW_BOARD_USR_WIFI := nanowifi
