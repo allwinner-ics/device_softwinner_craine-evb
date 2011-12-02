16013a59d394ad45d6273e28639065998ef14b5a
zhangxu <zhangxu@allwinnertech.com>
2011-12-02 17:15:55 +0800

    chang ui to pad and add usb mass storage support

--- crane_evb.mk
+++ crane_evb.mk
@@ -7,9 +7,10 @@ DEVICE_PACKAGE_OVERLAYS := device/softwinner/crane-evb/overlay
 PRODUCT_COPY_FILES += \
 	device/softwinner/crane-evb/ueventd.sun4i.rc:root/ueventd.sun4i.rc \
 	device/softwinner/crane-evb/init.sun4i.rc:root/init.sun4i.rc \
+	device/softwinner/crane-evb/init.sun4i.usb.rc:root/init.sun4i.usb.rc \
 	device/softwinner/crane-evb/media_profiles.xml:system/etc/media_profiles.xml \
-	device/softwinner/crane-evb/camera.cfg:system/etc/camera.cfg \
-
+	device/softwinner/crane-evb/camera.cfg:system/etc/camera.cfg 
+	
 PRODUCT_COPY_FILES += \
 	device/softwinner/crane-aino/initlogo.rle:root/initlogo.rle
 
@@ -20,9 +21,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
 	device/softwinner/crane-aino/initlogo.rle:root/initlogo.rle
-	
-PRODUCT_PACKAGES += \
-	TSCalibration2
 
 PRODUCT_COPY_FILES += \
 	frameworks/base/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
@@ -30,6 +28,13 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
 	device/softwinner/crane-evb/vold.fstab:system/etc/vold.fstab
+
+PRODUCT_PACKAGES += \
+	TSCalibration2
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mass_storage,adb \
+	ro.sf.lcd_density=120
 
 PRODUCT_CHARACTERISTICS := tablet
 
