b78ba2ab0c7880e2cbd993320b58502d3909af18
xiongtie <xiongtie@Exdroid3.(none)>
2011-12-26 20:31:27 +0800

    merge from tag exdroid4.0.1r1-v0.4

--- crane_evb.mk
+++ crane_evb.mk
@@ -5,15 +5,16 @@ $(call inherit-product, device/softwinner/crane-common/ProductCommon.mk)
 DEVICE_PACKAGE_OVERLAYS := device/softwinner/crane-evb/overlay
 
 PRODUCT_COPY_FILES += \
+	device/softwinner/crane-evb/kernel:kernel \
+	device/softwinner/crane-evb/recovery.fstab:recovery.fstab
+
+PRODUCT_COPY_FILES += \
 	device/softwinner/crane-evb/ueventd.sun4i.rc:root/ueventd.sun4i.rc \
 	device/softwinner/crane-evb/init.sun4i.rc:root/init.sun4i.rc \
 	device/softwinner/crane-evb/init.sun4i.usb.rc:root/init.sun4i.usb.rc \
 	device/softwinner/crane-evb/media_profiles.xml:system/etc/media_profiles.xml \
 	device/softwinner/crane-evb/camera.cfg:system/etc/camera.cfg 
 	
-PRODUCT_COPY_FILES += \
-	device/softwinner/crane-aino/initlogo.rle:root/initlogo.rle
-
 #input device config
 PRODUCT_COPY_FILES += \
 	device/softwinner/crane-evb/sun4i-keyboard.kl:system/usr/keylayout/sun4i-keyboard.kl \
@@ -23,18 +24,17 @@ PRODUCT_COPY_FILES += \
 	device/softwinner/crane-aino/initlogo.rle:root/initlogo.rle
 
 PRODUCT_COPY_FILES += \
-	frameworks/base/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
-
-PRODUCT_COPY_FILES += \
 	device/softwinner/crane-evb/vold.fstab:system/etc/vold.fstab
-
+	
 PRODUCT_PACKAGES += \
 	TSCalibration2
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.sys.usb.config=mass_storage,adb \
-	ro.sf.lcd_density=120
+	ro.sf.lcd_density=120 \
+	ro.product.firmware=0.3
+
+$(call inherit-product-if-exists, device/softwinner/crane-evb/modules/modules.mk)
 
 PRODUCT_CHARACTERISTICS := tablet
 
---
+++ package.sh
@@ -0,0 +1,3 @@
+cd $PACKAGE
+./pack -c sun4i -p crane -b evb 
+cd -
